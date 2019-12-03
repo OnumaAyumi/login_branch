35e00ec2e5463fe6ca585e16c3335fc53f0767f6
OnumaAyumi <onuayu.min@gmail.com>
2019-12-03 17:55:11 +0900

    変更点

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  resources :users do
+  resources :users ,except: :destroy do
     collection do 
       get "signup_new"
       get "phone"
