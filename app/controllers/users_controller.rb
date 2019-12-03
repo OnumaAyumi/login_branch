5f62ebdc8fddb85e5bd82f7d2f2bf88854308d46
OnumaAyumi <onuayu.min@gmail.com>
2019-12-03 17:32:58 +0900

    login-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def signup_new
+  end
+
+  def phone
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
+  resources :users do
+    collection do 
+      get "signup_new"
+      get "phone"
+      get "step3"
+    end
+  end
+
 end
