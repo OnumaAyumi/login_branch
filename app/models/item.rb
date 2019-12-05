3f6a0a97b56795177771c0a52267aa1e4f61ffed
shuheiyasuda <yasu.canes07@gmail.com>
2019-12-05 19:43:58 +0900

    変更点の改修

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :bland
   belongs_to :category
+
+  enum condition:[:"新品、未使用", :"未使用に近い",:"目立った傷や汚れなし"]
+  enum ship_way:[:"送料込み(出品者負担)", :"着払い(購入者負担)"]
+  enum ship_place:[:"未定", :"らくらくメルカリ便", :"ゆうメール", :"レターパック", :"普通郵便(定形、定形外)", :"クロネコヤマト", :"ゆうパック", :"クリックポスト", :"ゆうパケット"]
+  enum ship_date:[:"1~2日で発送", :"2~3日で発送", :"4~7日で発送"]
 end
--- db/migrate/20191122062041_create_items.rb
+++ db/migrate/20191122062041_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string  :ship_way
       t.string  :ship_place
       t.string  :ship_date
-      t.string  :condition
+      t.string :condition
       t.integer :price
       t.integer :place_id, foreign_key: true
       t.integer :bland_id, foreign_key: true
